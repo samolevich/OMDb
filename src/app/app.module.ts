9d70f6cc6ecbee2106bf27a28a7dbbba97fb7f44
samolevich <samolevich@gmail.com>
2020-10-01 19:01:55 +0300

    попытался разобраться с материал. не получилось

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,16 +8,9 @@ import { InputComponent } from './input/input.component';
 import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InputComponent,
-    ButtonComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, InputComponent, ButtonComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatFormFieldModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
